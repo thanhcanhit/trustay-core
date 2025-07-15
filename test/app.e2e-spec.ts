602ac4ab4625441b457743711f545103b065ad3d
thanhcanhit <thanhcanh.dev@gmail.com>
2025-07-15 11:18:50 +0700

    Refactor configuration files for biome and ESLint; remove Prettier integration. Update package.json scripts for biome formatting and linting. Ensure consistent formatting in JSON files.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { Test, type TestingModule } from "@nestjs/testing";
 import type { INestApplication } from "@nestjs/common";
+import { Test, type TestingModule } from "@nestjs/testing";
 import * as request from "supertest";
 import type { App } from "supertest/types";
 import { AppModule } from "./../src/app.module";
